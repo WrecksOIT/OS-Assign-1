ccfbf453a248dee7a8cb74e0bcdcdf213ecf0f54
Colin Rexing <colin.rexing@oit.edu>
2021-04-08 20:20:49 -0700

    initial start

---
+++ Assign-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assign_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var ring = new SafeRing(5);
+            ring.Insert(1);
+            ring.Insert(2);
+            ring.Insert(3);
+            ring.Remove();
+        }
+    }
+}
---
+++ Assign-1/SafeRing.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Assign_1
+{
+    public class SafeRing
+    {
+        private int[] _buffer;
+        private int _head;
+        private int _tail;
+        private int _capacity;
+        private int _size;
+        private readonly Mutex _mutex = new Mutex();
+        private ManualResetEvent _hasItems = new ManualResetEvent(false);
+        private ManualResetEvent _hasCapacity = new ManualResetEvent(true);
+
+        public SafeRing(int size)
+        {
+            _capacity = size;
+            _buffer = new int[size];
+        }
+
+        public int Remove()
+        {
+            WaitHandle.WaitAll(new WaitHandle[] {_mutex, _hasItems});
+
+            var i = _buffer[_head];
+            _head = (_head + 1) % _capacity;
+            _size--;
+            _hasCapacity.Set();
+            
+            if (_size == 0) 
+                _hasItems.Reset();
+
+            _mutex.ReleaseMutex();
+            return i;
+        }
+
+        public void Insert(int numberToInsert)
+        {
+            WaitHandle.WaitAll(new WaitHandle[] {_mutex, _hasCapacity});
+            _buffer[_tail] = numberToInsert;
+            _tail = (_tail + 1) % _capacity;
+            _size++;
+            _hasItems.Set();
+            
+            if (_size == _capacity) 
+                _hasCapacity.Reset();
+
+            _mutex.ReleaseMutex();
+        }
+
+        public int Count()
+        {
+            _mutex.WaitOne();
+            var result = _size;
+            _mutex.ReleaseMutex();
+            return result;
+        }
+    }
+}
